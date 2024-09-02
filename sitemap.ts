60e7c0a5313266d24471a404ee6be501e60b7fcc
Pere Solà Claver <pere.solaclaver@gmail.com>
2024-09-02 22:19:47 +0100

    Add www

--- sitemap.ts
+++ sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://peresola.com',
+      url: 'https://www.peresola.com',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 1,
