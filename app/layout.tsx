c4ff41518b80ceed6fc2229b0f3e31950ca1dd84
Pere Solà Claver <pere.solaclaver@gmail.com>
2024-09-14 17:29:45 +0100

    Install analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from 'next';
 import { Oswald } from 'next/font/google';
 import './globals.css';
+import Script from 'next/script';
 
 const oswald = Oswald({ subsets: ['latin'] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={oswald.className}>{children}</body>
+      <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
     </html>
   );
 }
